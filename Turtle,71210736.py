966ad82fec42c3acc21ee158a02be58f12c61841
ChristianNonti <93360281+ChristianNonti@users.noreply.github.com>
2022-12-15 16:22:03 +0700

    Add files via upload

---
+++ Turtle,71210736.py
@@ -0,0 +1,51 @@
+import turtle as t
+t.bgcolor("blue")
+# HURUF C
+t.pensize(10)
+t.up()
+t.goto(0,160)
+t.down()
+t.color("red")
+t.circle(55, -160)
+# HRUF N
+t.up()
+t.goto(20,-160)
+t.down()
+t.right(90)
+t.forward(120)
+t.backward(120)
+t.left(40)
+t.forward(155)
+t.left(139)
+t.forward(120)
+# Angka 7
+t.up()
+t.goto(-160,0)
+t.down()
+t.right(50)
+t.backward(100)
+t.right(50)
+t.forward(50)
+# Angka 3
+t.up()
+t.goto(0,50)
+t.down()
+t.circle(25, -160)
+t.backward(10)
+t.up()
+t.goto(0,50)
+t.down()
+t.circle(25, 160)
+# Angka 6
+t.up()
+t.goto(160,100)
+t.down()
+t.circle(50, 160)
+t.circle(20, 280)
+
+
+t.exitonclick()
+
+
+
+
